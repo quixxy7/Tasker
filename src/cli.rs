c7b4f5c6252dfc4d5cd43fe19c3a253b78269805
quixxy7 <quixxy7@gmail.com>
2026-05-19 17:43:51 +0700

    feat: add struct Cli and enum Commands

---
+++ src/cli.rs
@@ -0,0 +1,15 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    Add { name: String },
+    List,
+    Remove { id: u32 },
+}
