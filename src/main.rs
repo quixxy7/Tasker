f8c191a8aa4fe5f3a76c1c197175d6905a7b3bd1
quixxy7 <quixxy7@gmail.com>
2026-05-20 10:21:20 +0700

    feat: implement init, add, list, remove commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+mod cli;
+mod tasks;
+use clap::Parser;
+use cli::{Cli, Commands};
+use std::env;
+use std::fs;
+use tasks::{Task, TaskStorage};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Init => {
+            let mut path = env::current_dir().unwrap();
+            path.push(".tsk");
+            fs::create_dir(&path).unwrap();
+
+            path.push("tasks.json");
+            fs::write(path, "{\"next_id\": 1, \"tasks\": []}").unwrap();
+        }
+        Commands::Add { name } => {
+            let mut storage = TaskStorage::load();
+            let task = Task::new(storage.next_id, name);
+            storage.tasks.push(task);
+            storage.next_id += 1;
+            storage.save();
+        }
+        Commands::List => {
+            let storage = TaskStorage::load();
+            for task in storage.tasks.iter() {
+                println!("{:?}", task);
+            }
+        }
+        Commands::Remove { id } => {
+            let mut storage = TaskStorage::load();
+            for i in 0..storage.tasks.len() {
+                if storage.tasks[i].id == id {
+                    storage.tasks.remove(i);
+                }
+            }
+            storage.save();
+            //TODO Parse from JSON, Remove by ID, Send to JSON
+        }
+    }
 }
