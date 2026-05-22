fa0a7032791fbeff3eb5adc9dede0d81a0a3fc79
quixxy7 <quixxy7@gmail.com>
2026-05-22 21:13:13 +0700

    add id existence check for remove start done commands

--- src/main.rs
+++ src/main.rs
@@ -39,31 +39,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         Commands::Remove { id } => {
             let mut storage = TaskStorage::load()?;
+            let mut found: bool = false;
             for i in 0..storage.tasks.len() {
                 if storage.tasks[i].id == id {
+                    found = true;
                     storage.tasks.remove(i);
                     break;
                 }
+            }
+            if !found {
+                println!("Id has been not found. Task was not removed");
             }
             storage.save()?;
         }
         Commands::Start { id } => {
             let mut storage = TaskStorage::load()?;
+            let mut found: bool = false;
             for task in storage.tasks.iter_mut() {
                 if task.id == id {
+                    found = true;
                     task.status = TaskStatus::InProgress;
                     break;
                 }
+            }
+            if !found {
+                println!("Id has been not found. Task was not changed");
             }
             storage.save()?
         }
         Commands::Done { id } => {
             let mut storage = TaskStorage::load()?;
+            let mut found: bool = false;
             for task in storage.tasks.iter_mut() {
                 if task.id == id {
+                    found = true;
                     task.status = TaskStatus::Done;
                     break;
                 }
+            }
+            if !found {
+                println!("Id has been not found. Task was not changed");
             }
             storage.save()?
         }
